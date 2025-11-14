4a5246564dee5917bce83bb70a2db45def2562b5
Itebimien Momodu <fluxflareltd@gmail.com>
2025-11-14 16:28:50 +0100

    Create urls.py

---
+++ backend/geo_connect_backend/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path, include
+from apps.iam.views import LoginView, MeView
+from rest_framework_simplejwt.views import TokenRefreshView
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+
+    # Auth
+    path("api/v1/auth/login", LoginView.as_view(), name="auth-login"),
+    path("api/v1/auth/refresh", TokenRefreshView.as_view(), name="auth-refresh"),
+    path("api/v1/auth/me", MeView.as_view(), name="auth-me"),
+
+    # Admin APIs
+    path("api/v1/admin/", include("apps.admin_api.urls")),
+]
